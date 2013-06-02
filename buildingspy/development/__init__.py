518467a12f1af31ffc20c2efc3ba9d0e5d3b0f20
Michael Wetter <MWetter@lbl.gov>
2013-06-01 21:34:43 -0700

    Added validator.py

--- buildingspy/development/__init__.py
+++ buildingspy/development/__init__.py
@@ -1,5 +1,7 @@
 '''
 This module contains the class *Tester* that runs
-the unit tests of the `Buildings` library.
+the unit tests of the `Buildings` library, and the 
+class *Validator* that validates the html code of the info section
+of the ``.mo`` files.
 '''
 
