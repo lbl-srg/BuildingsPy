2e0feb68f052de396d782875ef8f6f19238ba38f
Michael Wetter <MWetter@lbl.gov>
2014-02-27 16:56:11 -0800

    Removed import that is not needed and added more tests.

--- buildingspy/tests/test_development_regressiontest.py
+++ buildingspy/tests/test_development_regressiontest.py
@@ -19,5 +19,38 @@ class Test_regressiontest_Tester(unittest.TestCase):
         os.remove('dymola.log')
         os.remove('unitTests.log')
 
+    def test_runSimulation(self):
+        import buildingspy.development.regressiontest as r
+        self.assertRaises(OSError, \
+                          r.runSimulation, ".", "this_command_does_not_exist")
+        
+    def test_areResultsEqual(self):
+        import buildingspy.development.regressiontest as r
+        rt = r.Tester()
+        tMin = 10
+        tMax = 50
+        nPoi = 101
+        tOld = [tMin, tMax]
+        yOld = [10, 10]
+        tNew = [tMin+float(i)/(nPoi-1)*(tMax-tMin) for i in range(nPoi) ]
+        yNew = [10.0 for i in range(nPoi)]
+        varNam = "testVariable"
+        filNam = "testFilename"
+        (equ, _, _) = rt.areResultsEqual(tOld, yOld, tNew, yNew, varNam, filNam)
+        self.assertTrue(equ, "Test 1 for equality failed.")
+        # Test with arguments reversed.
+        (equ, _, _) = rt.areResultsEqual(tNew, yNew, tOld, yOld, varNam, filNam)
+        self.assertTrue(equ, "Test 2 for equality failed.")
+        # Modify arguments, now both tests should return false
+        yNew[2] = 11.0 
+        (equ, timMaxErr, _) = rt.areResultsEqual(tOld, yOld, tNew, yNew, varNam, filNam)
+        self.assertFalse(equ, "Test 1 for equality should have returned false.")
+        self.assertEqual(tNew[2], timMaxErr, "Time of maximum error is wrong for test 1.")
+        # Test with arguments reversed.
+        (equ, timMaxErr, _) = rt.areResultsEqual(tNew, yNew, tOld, yOld, varNam, filNam)
+        self.assertFalse(equ, "Test 2 for equality should have returned false.")
+        self.assertEqual(tNew[2], timMaxErr, "Time of maximum error is wrong for test 2.")        
+        
+
 if __name__ == '__main__':
     unittest.main()
