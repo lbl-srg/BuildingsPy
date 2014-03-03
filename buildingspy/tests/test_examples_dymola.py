491e04d49e728e6e734c3fc48dcaea269b6b0edc
mbonvini <mbonvini@lbl.gov>
2014-03-03 14:33:03 -0800

    Now it is possible to specify the path of a modelica package without using the environmental variable

--- buildingspy/tests/test_examples_dymola.py
+++ buildingspy/tests/test_examples_dymola.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import unittest
+import os
 from buildingspy.io.postprocess import Plotter
 import numpy.testing
 
@@ -8,7 +9,20 @@ class Test_example_dymola_runSimulation(unittest.TestCase):
        This class contains the unit tests for
        :mod:`buildingspy.io.Plotter`.
     """
-
+    def setUp(self):
+        '''
+        This method creates an environmental variable that points to an existing folder
+        that contains a Modelica package.
+        '''
+        os.environ["MODELICAPATH"] = os.path.abspath(os.path.join("buildingspy", "tests", "MyModelicaLibrary"))
+    
+    def tearDown(self):
+        '''
+        This method delete the environmental variable that points to an existing folder
+        that contains a Modelica package.
+        '''
+        del(os.environ["MODELICAPATH"])
+        
     def test_runSimulation(self):
         '''
         Tests the :mod:`buildingspy/examples/dymola/runSimulation`
