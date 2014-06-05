001852ea1caf3ae9093e8e857650dcc7e7589103
Filip <filip.jorissen@gmail.com>
2014-06-05 14:34:24 +0200

    added fix for running from correct working directory after loading extra packages

--- buildingspy/development/regressiontest.py
+++ buildingspy/development/regressiontest.py
@@ -1219,8 +1219,13 @@ len(yNew)    = %d""" % (filNam, varNam, len(tGriOld), len(tGriNew), len(yNew)))
 
             if self._modelicaCmd == 'dymola':
                 runFil.write('openModel("package.mo");\n')
+                # store current work directory path before overwriting it by loading new packages
+                runFil.write('path = Modelica.Utilities.System.getWorkDirectory()\n')
                 for package in self._loadPackages:
                     runFil.write('openModel("' + package + '");\n')
+                # reset original work directory path
+                runFil.write('Modelica.Utilities.System.setWorkDirectory(path)\n')
+                
             elif self._modelicaCmd == 'omc':
                 runFil.write('loadModel(Modelica, {"3.2"});\n')
                 runFil.write('getErrorString();\n')
