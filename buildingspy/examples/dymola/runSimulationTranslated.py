72f6be555aaa258b6af1a10d7c50f5bc401e6736
Michael Wetter <MWetter@lbl.gov>
2015-10-27 23:01:44 -0700

    Updated documentation

--- buildingspy/examples/dymola/runSimulationTranslated.py
+++ buildingspy/examples/dymola/runSimulationTranslated.py
@@ -1,12 +1,12 @@
-from multiprocessing import Pool
 import buildingspy.simulate.Simulator as si
 
 
 # Function to set common parameters and to run the simulation
-def simulateCase(s):
-    ''' Set common parameters and run a simulation.
+def simulateTranslatedModel(s):
+    ''' Set common parameters and run a simulation
+        of an already translated model.
 
-    :param s: A simulator object.
+    :param s: A simulator object that has already been translated.
 
     '''
     s.setStopTime(86400)
@@ -20,6 +20,8 @@ def simulateCase(s):
 def main():
     ''' Main method that configures and runs all simulations
     '''
+    import copy
+    from multiprocessing import Pool
     # Build list of cases to run
     li = []
 
@@ -30,19 +32,20 @@ def main():
     s.addParameters({'con.eOn': 0.1})
     s.setSolver('dassl')
     s.showGUI(False)
+    # Translate the model
     s.translate()
+    # Add the model to the list of models to be simulated
     li.append(s)
 
     # Second model
-    import copy
     s2 = copy.deepcopy(s)
     s2.setOutputDirectory('case2')
     s2.addParameters({'con.eOn': 1})
     li.append(s2)
 
-    # Run all cases in parallel
+    # Run both models in parallel
     po = Pool()
-    po.map(simulateCase, li)
+    po.map(simulateTranslatedModel, li)
     # clean up
     s2.deleteTranslateDirectory()
 
