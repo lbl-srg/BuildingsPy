99fb9c711bcae70b412738cf88740110e7989a11
Michael Wetter <mwetter@lbl.gov>
2017-01-03 16:51:38 -0800

    Removed test for dynamic state selection as it is needed for Resources/Scripts/Dymola/Electrical/AC/ThreePhasesUnbalanced/Loads/Examples/LoadCtrl.mos

--- buildingspy/tests/test_development_error_dictionary.py
+++ buildingspy/tests/test_development_error_dictionary.py
@@ -33,8 +33,7 @@ class Test_development_error_dictionary(unittest.TestCase):
                       'type inconsistent definition equations',
                       'unspecified initial conditions',
                       'unused connector',
-                      'stateGraphRoot missing',
-                      'dynamic state selection'])
+                      'stateGraphRoot missing'])
 
         self.assertEqual(len(k), len(k_expected), "Wrong number of keys.")
         for i in range(len(k)):
@@ -57,8 +56,7 @@ class Test_development_error_dictionary(unittest.TestCase):
                       'Type inconsistent definition equation',
                       'Dymola has selected default initial condition',
                       'Warning: The following connector variables are not used in the model',
-                      "A \\\"stateGraphRoot\\\" component was automatically introduced.",
-                      'Dynamically selected continuous time states'])
+                      "A \\\"stateGraphRoot\\\" component was automatically introduced."])
 
         self.assertEqual(len(k), len(k_expected), "Wrong number of tool messages.")
         for i in range(len(k)):
