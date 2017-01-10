757e544f9f873d264b5d135a69cc51afa8533e59
thorade <matthis.thorade@gmail.com>
2017-01-10 12:44:52 +0100

    import from future,
    especially the print() function!
    this is for #94

--- buildingspy/development/fixParameters.py
+++ buildingspy/development/fixParameters.py
@@ -6,6 +6,11 @@
 #
 # Once you know the model name,
 
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+#from __future__ import unicode_literals
+
 import os
 import re
 import webbrowser
