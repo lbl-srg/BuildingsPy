54f0e60e5f5f9eb70dc02ca252c96de890a9ce36
thorade <matthis.thorade@gmail.com>
2017-02-03 18:51:54 +0100

    try re.sub instead

--- buildingspy/development/merger.py
+++ buildingspy/development/merger.py
@@ -96,7 +96,7 @@ class Annex60(object):
             lines = list()
             for _, lin in enumerate(f_sou):
                 for ori, new in rep.items():
-                    lin = str.replace(str(lin), ori, new)
+                    lin = re.sub(ori, new, lin)
                 lines.append(lin)
         # Write the lines to the new file
         f_des = open(destination_file, mode="w")
