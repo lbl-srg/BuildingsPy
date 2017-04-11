4e7e8f10b570f9e7dd3d00c8f72aaeaa16059fd9
Thierry Nouidui <tsnouidui@lbl.gov>
2017-04-11 15:35:49 -0700

    Removed assert

--- buildingspy/tests/test_development_check_parameters.py
+++ buildingspy/tests/test_development_check_parameters.py
@@ -5,14 +5,13 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
-from io import open
 
 import unittest
 
 class Test_development_check_parameters(unittest.TestCase):
     """
        This class contains the unit tests for
-       :mod:`buildingspy.development.Validator`.
+       :mod:`buildingspy.development.check_parameters.
     """
 
     def test_validateModelParameters(self):
@@ -20,11 +19,7 @@ class Test_development_check_parameters(unittest.TestCase):
         import buildingspy.development.check_parameters as v
         val = v.Validator()
         myMoLib = os.path.join("buildingspy", "tests", "MyModelicaLibrary")
-        # Get a list whose elements are the error strings
-        retVal = val.validateModelParameters(myMoLib)
-        #self.assertEqual(len(errStr), 0)
-        # Test a package that does not exist
-        #self.assertRaises(ValueError, val.validateHTMLInPackage, "non_existent_modelica_package")
+        val.validateModelParameters(myMoLib)
 
 if __name__ == '__main__':
     unittest.main()
