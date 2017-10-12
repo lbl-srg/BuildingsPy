04759664342cb8dbc5a982096c140f9efcb82aa9
thorade <matthis.thorade@gmail.com>
2017-10-12 13:58:51 +0200

    sort the keys when dumping json
    also address #175

--- buildingspy/development/regressiontest.py
+++ buildingspy/development/regressiontest.py
@@ -2351,8 +2351,11 @@ getErrorString();
                         retVal = 1
                 # Dump an array of testCase objects
                 # dump to a string first using json.dumps instead of json.dump
-                json_string = json.dumps({"testCase": stat}, indent=4,
-                                         separators=(',', ': '), ensure_ascii=False)
+                json_string = json.dumps({"testCase": stat},
+                                         ensure_ascii=False,
+                                         indent=4,
+                                         separators=(',', ': '),
+                                         sort_keys=True)
                 logFil.write(json_string)
 
         # check logfile if omc
