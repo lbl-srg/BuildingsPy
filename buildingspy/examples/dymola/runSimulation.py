1cae73d32d75a61c92851a6c9072ae33b9373523
Michael Wetter <mwetter@lbl.gov>
2020-05-17 08:17:41 -0700

    Revised examples

--- buildingspy/examples/dymola/runSimulation.py
+++ buildingspy/examples/dymola/runSimulation.py
@@ -13,7 +13,8 @@ from io import open
 # end of from future import
 
 from multiprocessing import Pool
-import buildingspy.simulate.Dymola as si
+
+from buildingspy.simulate.Dymola import Dymola
 
 
 # Function to set common parameters and to run the simulation
@@ -26,7 +27,6 @@ def simulateCase(s):
     s.setStopTime(86400)
     # Kill the process if it does not finish in 1 minute
     s.setTimeOut(60)
-    s.showProgressBar(False)
     s.printModelAndTime()
     s.simulate()
 
@@ -35,15 +35,16 @@ def main():
     """ Main method that configures and runs all simulations
     """
     import shutil
+
     # Build list of cases to run
     li = []
     # First model
     model = 'Buildings.Controls.Continuous.Examples.PIDHysteresis'
-    s = si.Simulator(model, 'dymola', 'case1')
+    s = Dymola(model, 'case1')
     s.addParameters({'con.eOn': 0.1})
     li.append(s)
     # second model
-    s = si.Simulator(model, 'dymola', 'case2')
+    s = Dymola(model, 'case2')
     s.addParameters({'con.eOn': 1})
     li.append(s)
 
