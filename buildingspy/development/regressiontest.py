8fc871a5b4dbd139c3d12a0a0f4463029dd646d0
Michael Wetter <mwetter@lbl.gov>
2021-01-08 12:24:41 -0800

    Excluded files from being copied during regression tests

--- buildingspy/development/regressiontest.py
+++ buildingspy/development/regressiontest.py
@@ -3298,11 +3298,14 @@ class Tester(object):
                     '.mat',
                     'request.',
                     'status.',
+                    'dsmodel.c',
+                    'dymosim',
                     'tmp-*',
                     'funnel-comp',
                     'fmi-library',  # Not all of src is excluded as some .mo models link to files from src
                     'Documentation',
                     'ReferenceResults',
+                    'help',
                     'compareVars',
                     '__pychache__'))
         return
