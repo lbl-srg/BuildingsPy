3ebff8ef8952e46d700cee90d1fa9b702ab81af5
AntoineGautier <agautier@lbl.gov>
2021-03-23 17:39:22 -0700

    Update pyfunnel version

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setup(
         'scipy>=1.1',
         'simplejson>=3.14',
         'six>=1.11',
-        'pyfunnel @ git+https://github.com/lbl-srg/funnel@issue49_localTolerance#egg=pyfunnel',
+        'pyfunnel>=0.3.0',
     ],
     packages=[
         MAIN_PACKAGE,
