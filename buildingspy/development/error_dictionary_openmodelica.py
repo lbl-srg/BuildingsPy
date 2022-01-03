a8a26ca3f59c7cef52d07209da7907290aea5bea
Michael Wetter <mwetter@lbl.gov>
2022-01-03 13:50:18 -0800

    Removed test in OpenModelica. See comment in development/error_dictionary_openmodelica.py

--- buildingspy/development/error_dictionary_openmodelica.py
+++ buildingspy/development/error_dictionary_openmodelica.py
@@ -25,10 +25,15 @@ class ErrorDictionary(ed.ErrorDictionary):
         self._error_dict = dict()
         # Set the error dictionaries.
         # Note that buildingspy_var needs to be a unique variable name.
-        self._error_dict["each applied to scalar"] = {
-            'tool_message': "used when modifying non-array element",
-            'counter': 0,
-            'summary_message': "Number of models with 'each' keyword applied to scalar       : {}\n"}
+
+        # The test below is commented as OpenModelica issues a warning for example in
+        # Buildings.Fluid.MixingVolumes.Validation.MixingVolumeHeatReverseFlow.
+        # This is already reported in https://github.com/OpenModelica/OpenModelica/issues/7777
+        # and in https://github.com/modelica/ModelicaSpecification/issues/3052
+        ##self._error_dict["each applied to scalar"] = {
+        ##    'tool_message': "used when modifying non-array element",
+        ##    'counter': 0,
+        ##    'summary_message': "Number of models with 'each' keyword applied to scalar       : {}\n"}
 
         self._error_dict["assuming each"] = {
             'tool_message': "Non-array modification",
