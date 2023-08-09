cd6375cb8ec22050bb1ecf3f6b6ab9a5ae262255
Michael Wetter <mwetter@lbl.gov>
2023-08-09 16:45:10 -0700

    Corrected output directory

--- buildingspy/simulate/OpenModelica.py
+++ buildingspy/simulate/OpenModelica.py
@@ -211,7 +211,7 @@ class Simulator(bs._BaseSimulator):
             template = env.get_template("openmodelica_run.template")
 
             txt = template.render(
-                working_directory=self._outputDir_,
+                working_directory='.',
                 library_name=self.modelName.split(".")[0],
                 package_path=self.getPackagePath(),
                 model=self.modelName,
