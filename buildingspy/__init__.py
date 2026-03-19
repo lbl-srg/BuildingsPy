fe8cbf22ef2e4e7e018275c0a0c536b6842a0190
Michael Wetter <mwetter@lbl.gov>
2026-03-19 06:57:12 -0700

    Run autopep

--- buildingspy/__init__.py
+++ buildingspy/__init__.py
@@ -10,6 +10,7 @@ version_path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'VERSION'
 with open(version_path) as f:
     __version__ = f.read().strip()
 
+
 class BuildingsPy:
     """Class with static methods that are used in various modules.
 
